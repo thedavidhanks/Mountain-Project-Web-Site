1e6f64e4d0680c852e56b8bf119d20de7684c444
briantowner <116382436+briantowner@users.noreply.github.com>
2023-01-25 16:18:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+URL = 'https://www.mountainproject.com/'
+response = requests.get(url=URL)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+data = soup.select('div#route-guide strong a')
+
+state_links =[]
+area_links = []
+for item in data:
+    if item['href'] not in state_links:
+        state_links.append(item['href'])
+
+for item in state_links[:3]:
+    response = requests.get(url=item)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data = soup.select('div.lef-nav-row a')
+    for link in data:
+        area_links.append(link['href'])
+pprint(area_links)
